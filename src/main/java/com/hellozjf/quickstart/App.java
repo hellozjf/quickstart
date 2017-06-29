90260fed580c1a5eea0cd37402b2423f68a4bdbe
hellozjf <908686171@qq.com>
2017-06-29 21:41:24 +0800

    Update quickstart

---
+++ src/main/java/com/hellozjf/quickstart/App.java
@@ -0,0 +1,11 @@
+package com.hellozjf.quickstart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
